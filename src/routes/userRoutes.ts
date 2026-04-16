f8447ac8d12d5b9914e10ef2d2a97ddba408439a
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-16 19:49:39 +0530

    Added routes and attached handlers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,3 +1,17 @@
 import { Router } from "express";
+import { login, logout, protect, restrictTo, signup } from "../controllers/authController.js";
+import { createUser, deleteUser, getUsers, updateUser } from "../controllers/userController.js";
 
 export const userRouter = Router();
+
+userRouter.post("/signup", signup);
+userRouter.post("login", login);
+
+userRouter.use(protect);
+userRouter.get("/logout", logout);
+userRouter
+  .route("/")
+  .get(restrictTo("admin", "manager"), getUsers)
+  .post(restrictTo("admin"), createUser);
+
+userRouter.route("/:userId").patch(updateUser).delete(deleteUser);
