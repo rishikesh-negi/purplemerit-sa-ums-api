f86cfd5cef0b9d691f08394ac403cb65bfbdea93
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-16 19:46:34 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+import mongoose from "mongoose";
+import app from "./app.js";
+import { gracefulShutdown, shutdown } from "./utils/gracefulShutdown.js";
+
+process.on("uncaughtException", (err) => {
+  console.log("🔴 Unhandled exception encountered! Shutting down...");
+  console.log(`${err.name}: ${err.message}`);
+});
+
+const DB = process.env["DATABASE"]?.replace("<DB_PASSWORD", process.env["DB_PASSWORD"]!);
+
+mongoose
+  .connect(DB!)
+  .then(
+    () => process.env["NODE_ENV"] === "development" && console.log("DB successfully connected"),
+  );
+const PORT: number = +process.env["PORT"]! || 8000;
+
+export const server = app.listen(
+  PORT,
+  "0.0.0.0",
+  () => process.env["NODE_ENV"] === "development" && console.log(`App running on port ${PORT}`),
+);
+
+process.on("unhandledRejection", (err: Error) => {
+  gracefulShutdown(err, `🔴 Unhandled rejection encountered! Shutting down...`, server);
+});
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
