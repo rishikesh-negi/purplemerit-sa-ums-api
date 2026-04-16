9b5864f61c3888bc3b943301c6992eff1c7f64cb
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-17 01:02:02 +0530

    Added the route to refresh session

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
-import { login, logout, protect, restrictTo, signup } from "../controllers/authController.js";
+import {
+  login,
+  logout,
+  protect,
+  refreshSession,
+  restrictTo,
+  signup,
+} from "../controllers/authController.js";
 import { createUser, deleteUser, getUsers, updateUser } from "../controllers/userController.js";
 
 export const userRouter = Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
+userRouter.get("/refresh-session", refreshSession);
+userRouter.get("/logout", logout);
 
 userRouter.use(protect);
-userRouter.get("/logout", logout);
 userRouter
   .route("/")
   .get(restrictTo("admin", "manager"), getUsers)
