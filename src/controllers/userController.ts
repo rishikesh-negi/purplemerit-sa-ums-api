1b4a753a4419d95718f251eb83e4b09ab6e6a777
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-16 19:50:10 +0530

    Created a controller file for all user route handlers

---
+++ src/controllers/userController.ts
@@ -0,0 +1,156 @@
+import mongoose from "mongoose";
+import {
+  BadRequestError,
+  ForbiddenAccessError,
+  UnauthorizedAccessError,
+  UserNotFoundError,
+} from "../errors/AppError.js";
+import { User } from "../models/UserModel.js";
+import { catchAsyncError } from "../utils/catchAsyncError.js";
+
+export const getUsers = catchAsyncError(async (req, res) => {
+  type QueryParams = { page?: string; limit?: string; search?: string };
+
+  const { page: queryPage = 1, limit: queryLimit = 10, search = "" } = req.query as QueryParams;
+  const page = Number(queryPage);
+  const limit = Number(queryLimit);
+  let users;
+
+  if (search !== "") {
+    users = await User.aggregate([
+      {
+        $search: {
+          index: "default",
+          text: {
+            query: search,
+            path: ["firstName", "lastName", "username", "email"],
+            fuzzy: {
+              maxEdits: 2,
+              prefixLength: 1,
+            },
+          },
+        },
+      },
+      {
+        $skip: (page - 1) * limit,
+      },
+      {
+        $limit: limit,
+      },
+    ]);
+  }
+
+  if (search === "") {
+    users = await User.find()
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+  }
+
+  res.status(200).json({
+    users,
+  });
+});
+
+export const createUser = catchAsyncError(async (req, res, next) => {
+  const adminId = req.user?.id;
+  if (!adminId) return next(new UnauthorizedAccessError());
+  const { firstName, lastName, username, email, role, password } = req.body;
+  const newUser = await User.create({
+    firstName,
+    lastName,
+    username,
+    email,
+    role,
+    createdBy: adminId,
+    password,
+  });
+  newUser.createdBy = mongoose.Types.ObjectId.createFromHexString(adminId);
+  newUser.save();
+  return res.sendStatus(201);
+});
+
+export const updateUser = catchAsyncError(async (req, res, next) => {
+  const targetUserId = req.params["userId"];
+  const actionCreatorId = req.user!.id;
+  const actionCreatorRole = req.user!.role;
+
+  async function getUpdatedUserDoc(userId: string, updatedData: Record<string, unknown>) {
+    const updatedUser = await User.findByIdAndUpdate(userId, updatedData, {
+      new: true,
+      runValidators: true,
+    });
+
+    return updatedUser;
+  }
+
+  if (actionCreatorRole === "admin" || actionCreatorId === targetUserId) {
+    const updatedUser = (await getUpdatedUserDoc(targetUserId as string, req.body))!;
+    if (actionCreatorRole === "admin" && actionCreatorId !== targetUserId) {
+      updatedUser.updatedAt = new Date();
+      updatedUser.updatedBy = mongoose.Types.ObjectId.createFromHexString(actionCreatorId);
+      updatedUser.save();
+    }
+    return res.status(200).json({
+      user: updatedUser,
+    });
+  }
+
+  if (actionCreatorRole === "manager") {
+    const { role: targetUserRole } = (await User.findById(targetUserId).select("role").lean()) as {
+      role: "user" | "manager" | "admin";
+    };
+    if (!targetUserRole) return next(new BadRequestError());
+    if (targetUserRole === "admin") return next(new ForbiddenAccessError());
+
+    const updatedUser = (await getUpdatedUserDoc(targetUserId as string, req.body))!;
+
+    if (actionCreatorId !== targetUserId) {
+      updatedUser.updatedAt = new Date();
+      updatedUser.updatedBy = mongoose.Types.ObjectId.createFromHexString(actionCreatorId);
+      updatedUser.save();
+    }
+
+    return res.status(200).json({
+      user: updatedUser,
+    });
+  }
+
+  return next(new ForbiddenAccessError());
+});
+
+export const deleteUser = catchAsyncError(async (req, res, next) => {
+  const targetUserId = req.params["userId"];
+  const actionCreatorId = req.user?.id;
+  const actionCreatorRole = req.user?.role;
+
+  if (actionCreatorRole === "admin" || actionCreatorId === targetUserId) {
+    const user = await User.findById(targetUserId);
+    if (!user) return next(new UserNotFoundError());
+
+    await user.updateOne({ active: false });
+    return res.sendStatus(204);
+  }
+
+  return next(new ForbiddenAccessError());
+});
+
+export const getUser = catchAsyncError(async (req, res, next) => {
+  const userId = req.params["userId"];
+  const actionCreatorId = req.user!.id;
+  const actionCreatorRole = req.user!.role;
+  if (!userId) return next(new BadRequestError());
+
+  if (
+    actionCreatorRole === "admin" ||
+    actionCreatorRole === "manager" ||
+    actionCreatorId === userId
+  ) {
+    const user = await User.findById(userId).select("+createdAt +updatedAt");
+    if (!user) return next(new UserNotFoundError());
+
+    res.status(200).json({
+      user,
+    });
+  }
+});
