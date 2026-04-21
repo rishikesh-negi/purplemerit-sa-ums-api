bd5de731f95903cdea26f041c552adb4824975ba
Rishikesh Negi <rishinegi97@gmail.com>
2026-04-21 18:36:50 +0530

    Changed cookie options to match the options set on sent cookies

--- src/utils/removeClientRefreshTokenCookie.ts
+++ src/utils/removeClientRefreshTokenCookie.ts
@@ -3,8 +3,11 @@ import { REFRESH_JWT_COOKIE_NAME } from "./constants.js";
 
 export const removeClientRefreshTokenCookie = (req: Request, res: Response) => {
   res.clearCookie(REFRESH_JWT_COOKIE_NAME, {
+    secure:
+      process.env["NODE_ENV"] === "development"
+        ? false
+        : req.secure || req.headers["x-forwarded-proto"] === "https",
     httpOnly: true,
-    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-    sameSite: "strict",
+    sameSite: process.env["NODE_ENV"] === "development" ? "lax" : "strict",
   });
 };
